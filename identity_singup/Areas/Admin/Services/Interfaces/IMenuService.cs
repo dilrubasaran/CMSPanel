5e95d69c6876bb628403c905d5ed406020ff7c4e
Dilruba Başaran <69626494+dilrubasaran@users.noreply.github.com>
2025-03-06 22:57:48 +0300

    refactor: IMenuService arayüzünü ayrı bir dosyaya taşı ve debug loglarını kaldır

--- identity_singup/Areas/Admin/Services/Interfaces/IMenuService.cs
+++ identity_singup/Areas/Admin/Services/Interfaces/IMenuService.cs
@@ -1,4 +1,9 @@
-// public interface IMenuService
-// {
-//     Task<List<MenuItemViewModel>> GetMenuItemsForUserAsync(string userId);
-// }
+using identity_singup.Models;
+using System.Security.Claims;
+
+public interface IMenuService
+{
+    Task<List<MenuItem>> GetMenuItemsByClaims(IEnumerable<Claim> claims);
+    Task<List<MenuItem>> GetMenuItemsByRole(string role);
+    string GetMenuUrl(MenuItem menuItem);
+}
